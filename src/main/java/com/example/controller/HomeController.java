6f09b5a2c7393fc332ff60d3b9b184b53d3ce886
Adityaid808 <adityavairagade808@gmail.com>
2022-02-27 23:20:51 +0530

    temp2_chnges

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -28,6 +28,7 @@ public class HomeController {
 	{
         System.out.println("27-02-2022");
         System.out.println("inside home 1 controller 2");
+        System.out.println("done git");
 		map.addAttribute("timestamp",LocalDateTime.now());
 		
 		map.addAttribute("arraylist",Arrays.asList(10,56,72,36,45));
