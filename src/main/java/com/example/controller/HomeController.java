565a9423b7e6a5c903e88a8742ca0b21951302ac
Adityaid808 <adityavairagade808@gmail.com>
2022-02-28 00:28:52 +0530

    last

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -35,6 +35,8 @@ public class HomeController {
 		System.out.println(1223344567);
 		System.out.println(0000);
 		System.out.println("last");
+		System.out.println("1223344uhuhhuhuhuh567");
+
 		map.addAttribute("arraylist",Arrays.asList(10,56,72,36,45));
 		return "welcome1";
 	}
